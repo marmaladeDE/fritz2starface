39b4c7061b5568d1a426ca8ff8286bcf27a8d29e
Joscha Krug <krug@marmalade.de>
2013-11-24 21:58:46 +0100

    Finished Export

--- fritz2starface.php
+++ fritz2starface.php
@@ -1,5 +1,7 @@
 <?php
 
+work();
+
 function getFritzboxData()
 {
     $path = __DIR__.'/fritzbox.xml';
@@ -39,4 +41,78 @@ function selectNumbers( $phonenode )
         }
     }
     return $aNumbers;
+}
+
+function generateFullEntry( $aContact )
+{
+    $aEntry =  array();
+    $aEntry['Vorname [contact:firstname]']              = '';
+    $aEntry['Name [contact:familyname]']                = $aContact['name'];
+    $aEntry['Firma [contact:company]']                  = '';
+    $aEntry['Straße [address:street]']                  = '';
+    $aEntry['PLZ [address:postcode]']                   = '';
+    $aEntry['Stadt [address:city]']                     = '';
+    $aEntry['Bundesland [address:state]']               = '';
+    $aEntry['Rufnummer [telephone:phone]']              = $aContact['work'];
+    $aEntry['Rufnummer [telephone:short dial(phone)]']  = '';
+    $aEntry['Privat [telephone:homephone]']             = $aContact['home'];
+    $aEntry['Privat [telephone:short dial(homephone)]'] = '';
+    $aEntry['Mobil [telephone:mobile]']                 = $aContact['mobile'];
+    $aEntry['Mobil [telephone:short dial(mobile)]']     = '';
+    $aEntry['Fax [telephone:fax]']                      = '';
+    $aEntry['Fax [telephone:short dial(fax)]']          = '';
+    $aEntry['E-Mail [email:e-mail]']                    = '';
+    $aEntry['URL [email:url]']                          = '';
+
+    return $aEntry;
+
+}
+
+function exportToCsv( $aData )
+{
+    $fh = fopen( 'starface.csv', 'w' );
+    
+    $header = array(
+                'Vorname [contact:firstname]',
+                'Name [contact:familyname]',
+                'Firma [contact:company]',
+                'Straße [address:street]',
+                'PLZ [address:postcode]',
+                'Stadt [address:city]',
+                'Bundesland [address:state]',
+                'Rufnummer [telephone:phone]',
+                'Rufnummer [telephone:short dial(phone)]',
+                'Privat [telephone:homephone]',
+                'Privat [telephone:short dial(homephone)]',
+                'Mobil [telephone:mobile]',
+                'Mobil [telephone:short dial(mobile)]',
+                'Fax [telephone:fax]',
+                'Fax [telephone:short dial(fax)]',
+                'E-Mail [email:e-mail]',
+                'URL [email:url]'
+        );
+    
+    fputcsv( $fh, $header, ';');
+    
+    foreach ( $aData as $row ) {
+        fputcsv( $fh, $row, ';');
+    }
+
+    fclose($fh);
+}
+
+function work()
+{
+    $aSourceData = getFritzboxData();
+    
+    $aFullData = array();
+    
+    foreach( $aSourceData as $aContact)
+    {
+        $aFullData[] = generateFullEntry( $aContact );
+    }
+    
+    exportToCsv( $aFullData );
+    
+    echo "Finished export!\n<br><a href='starface.csv'>Download the starface.csv</a>";
 }
